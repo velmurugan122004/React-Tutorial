1bda413b6affae48d341280a36cd4acfd9f3ec67
velmurugan122004 <velmurugan12012004@gmail.com>
2026-04-14 12:21:27 +0530

    create multiple page using -router

--- Ecommerce-project/src/App.jsx
+++ Ecommerce-project/src/App.jsx
@@ -1,9 +1,14 @@
+import {Routes ,Route} from 'react-router'
 import { HompePages } from '../pages/HomePages'
 import './App.css'
 
 function App() {
   return (
-    <HompePages />
+    <Routes>
+      {/*<Route path='/' element={<HompePages />}></Route> this same of below*/}
+      <Route index element={<HompePages />} />
+      <Route path='checkout' element={<div>Vels Test page </div>}></Route>
+    </Routes>
   )
 }
 
