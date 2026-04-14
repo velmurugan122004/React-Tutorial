3648b0702fb343ed971c05b2e0a7eef6ebb5847b
velmurugan122004 <velmurugan12012004@gmail.com>
2026-04-14 13:18:07 +0530

    Split Header into a component and use Link to connect all pages

--- Ecommerce-project/pages/Tracking.jsx
+++ Ecommerce-project/pages/Tracking.jsx
@@ -1,39 +1,10 @@
 import './Tracking.css';
+import { Header } from '../components/Header';
 export function Tracking(){
   return (
     <>
       <title>Tracking</title>
-      <div className="header">
-      <div className="left-section">
-        <a href="/" className="header-link">
-          <img className="logo"
-            src="images/logo-white.png" />
-          <img className="mobile-logo"
-            src="images/mobile-logo-white.png" />
-        </a>
-      </div>
-
-      <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
-
-        <button className="search-button">
-          <img className="search-icon" src="images/icons/search-icon.png" />
-        </button>
-      </div>
-
-      <div className="right-section">
-        <a className="orders-link header-link" href="/">
-
-          <span className="orders-text">Orders</span>
-        </a>
-
-        <a className="cart-link header-link" href="/">
-          <img className="cart-icon" src="images/icons/cart-icon.png" />
-          <div className="cart-quantity">3</div>
-          <div className="cart-text">Cart</div>
-        </a>
-      </div>
-      </div>
+      <Header />
 
       <div className="tracking-page">
         <div className="order-tracking">
