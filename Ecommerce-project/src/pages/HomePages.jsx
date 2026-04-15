0bddcb5c0ce9f3da05e894a869d292aef62a65e9
velmurugan122004 <velmurugan12012004@gmail.com>
2026-04-15 13:53:40 +0530

    add server proxy confiq for /api and /images

--- Ecommerce-project/src/pages/HomePages.jsx
+++ Ecommerce-project/src/pages/HomePages.jsx
@@ -13,12 +13,12 @@ export function HomePages(){
       console.log(data);
   }); instead of uing above to below code*/
   useEffect(()=>{
-    axios.get('http://localhost:3000/api/products')
+    axios.get('/api/products')
     .then((response)=>{
     //console.log(response.data);
     setProducts(response.data);
     });
-    axios.get('http://localhost:3000/api/cart-items').then((response)=>{
+    axios.get('/api/cart-items').then((response)=>{
       console.log(response.data);
       setCart(response.data);
     });
